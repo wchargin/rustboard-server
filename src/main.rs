4bdd9f885873678ae0f4693b95fb40bda33537f6
William Chargin <wchargin@gmail.com>
2020-10-11 21:36:13 -0700

    server: add basic gRPC server with `ListRuns`
    Test Plan:
    Tonic doesn’t yet have server reflection support (it’s on the roadmap),
    so `grpc_cli ls` doesn’t work, but `grpc_cli call` still works:
    
    ```
    $ grpc_cli --channel_creds_type=insecure --proto_path proto/ --protofiles data_provider.proto call localhost:6206 TensorBoardDataProv
    ider.ListRuns ""
    connecting to localhost:6206
    Received initial metadata from server:
    date : Mon, 12 Oct 2020 04:37:35 GMT
    runs {
      id: "lr_1E-03,conv=2,fc=2"
      name: "lr_1E-03,conv=2,fc=2"
      start_time: 1563406405
    }
    runs {
      id: "lr_1E-03,conv=1,fc=2"
      name: "lr_1E-03,conv=1,fc=2"
      start_time: 1563406327
    }
    runs {
      id: "lr_1E-04,conv=2,fc=2"
      name: "lr_1E-04,conv=2,fc=2"
      start_time: 1563406600
    }
    runs {
      id: "lr_1E-04,conv=1,fc=2"
      name: "lr_1E-04,conv=1,fc=2"
      start_time: 1563406522
    }
    Rpc succeeded with OK status
    Reflection request not implemented; is the ServerReflection service enabled?
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,104 @@
-use std::path::PathBuf;
-
-use rustboard_server::commit::{self, Commit};
+use futures_core::Stream;
 use rustboard_server::logdir::LogdirLoader;
+use std::default::Default;
+use std::path::PathBuf;
+use std::pin::Pin;
+use std::thread;
+use std::time::Duration;
+use tonic::{transport::Server, Request, Response, Status};
 
-fn main() {
+use rustboard_server::commit::Commit;
+use rustboard_server::proto::tensorboard::data;
+
+use data::tensor_board_data_provider_server as tbdps;
+use tbdps::{TensorBoardDataProvider, TensorBoardDataProviderServer};
+
+const RELOAD_INTERVAL: Duration = Duration::from_secs(5);
+
+struct DataProvider<'a> {
+    head: &'a Commit,
+}
+
+#[tonic::async_trait]
+// TODO(@wchargin): Figure out if we can make this broader than 'static.
+impl TensorBoardDataProvider for DataProvider<'static> {
+    async fn list_runs(
+        &self,
+        _request: Request<data::ListRunsRequest>,
+    ) -> Result<Response<data::ListRunsResponse>, Status> {
+        let mut res = data::ListRunsResponse::default();
+        let start_times = self.head.start_times.read();
+        for (run_name, start_time) in &*start_times {
+            res.runs.push(data::Run {
+                id: run_name.clone(),
+                name: run_name.clone(),
+                start_time: *start_time,
+            });
+        }
+        Ok(Response::new(res))
+    }
+
+    async fn list_scalars(
+        &self,
+        _request: Request<data::ListScalarsRequest>,
+    ) -> Result<Response<data::ListScalarsResponse>, Status> {
+        todo!()
+    }
+
+    async fn read_scalars(
+        &self,
+        _request: Request<data::ReadScalarsRequest>,
+    ) -> Result<Response<data::ReadScalarsResponse>, Status> {
+        todo!()
+    }
+
+    async fn list_blob_sequences(
+        &self,
+        _request: Request<data::ListBlobSequencesRequest>,
+    ) -> Result<Response<data::ListBlobSequencesResponse>, Status> {
+        todo!()
+    }
+
+    async fn read_blob_sequences(
+        &self,
+        _request: Request<data::ReadBlobSequencesRequest>,
+    ) -> Result<Response<data::ReadBlobSequencesResponse>, Status> {
+        todo!()
+    }
+
+    type ReadBlobStream =
+        Pin<Box<dyn Stream<Item = Result<data::ReadBlobResponse, Status>> + Send + Sync + 'static>>;
+
+    async fn read_blob(
+        &self,
+        _request: Request<data::ReadBlobRequest>,
+    ) -> Result<Response<Self::ReadBlobStream>, Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let logdir = std::env::args_os()
         .nth(1)
         .expect("give logdir as first argument");
-    let commit = Commit::new();
-    let mut loader = LogdirLoader::new(&commit, PathBuf::from(logdir));
-    eprintln!("initialized loader");
-    loader.reload();
 
-    fn describe_store<V>(name: &str, store: &commit::Store<V>) {
-        for (run, tag_map) in &*store.read() {
-            eprintln!("have {} for {:?}", name, run);
-            for (tag, ts) in &*tag_map.read() {
-                eprintln!(
-                    "run {:?} tag {:?} (plugin={}) has {} points up to step {}",
-                    run,
-                    tag,
-                    ts.metadata
-                        .plugin_data
-                        .as_ref()
-                        .map(|pd| pd.plugin_name.as_str())
-                        .unwrap_or("???"),
-                    ts.values.len(),
-                    ts.values.last().unwrap().0
-                );
+    // leak commit because gRPC server requires static lifetime
+    let commit: &'static Commit = Box::leak(Box::new(Commit::new()));
+
+    thread::Builder::new()
+        .name("Reloader".into())
+        .spawn(move || {
+            let mut loader = LogdirLoader::new(commit, PathBuf::from(logdir));
+            loop {
+                loader.reload();
+                thread::sleep(RELOAD_INTERVAL);
             }
-        }
-    }
+        })?;
 
-    describe_store("scalars", &commit.scalars);
-    describe_store("tensors", &commit.tensors);
-    describe_store("blobs", &commit.blob_sequences);
-
-    eprintln!("terminating");
+    let addr = "[::1]:6206".parse().unwrap();
+    let dp = DataProvider { head: commit };
+    let svc = TensorBoardDataProviderServer::new(dp);
+    Server::builder().add_service(svc).serve(addr).await?;
+    Ok(())
 }
